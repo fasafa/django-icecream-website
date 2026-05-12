8b00d961c9ba08020cb421ded1d5bb88f2909e46
SAFA P S <104003591+fasafa@users.noreply.github.com>
2026-05-13 00:26:43 +0530

    Update settings.py

--- NewProject/settings.py
+++ NewProject/settings.py
@@ -27,11 +27,13 @@ SECRET_KEY = 'django-insecure-u*o#j547vusnz26nxa7v8-i7zj51k66hy_0m_y1pmnsoc(aa1)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    ".vercel.app",
-    "localhost",
-    "127.0.0.1",
-]
+# ALLOWED_HOSTS = [
+#     ".vercel.app",
+#     "localhost",
+#     "127.0.0.1",
+# ]
+
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
